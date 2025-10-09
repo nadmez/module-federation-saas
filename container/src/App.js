e11a7b9a8c9eabc4905fe85dbe0d81cf98f28e56
nadmez <nadmez@yahoo.com>
2025-10-09 16:18:51 +0300

    Marketing app integration starter

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import MarketingApp from "../components/MarketingApp";
 
 const App = () => {
   console.log("Hello from Container");
-  return <h1>Hello from Container</h1>;
+  return (
+    <div>
+      <h1>Hello from Container</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
 
 export default App;
